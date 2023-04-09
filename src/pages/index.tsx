12685f73774a1f7970a6d308aeb8bc7d88d4f992
Irving Arreola Palacios <irving.a789@gmail.com>
2023-04-09 05:49:18 -0500

    added topics to content page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
+import { useState } from "react";
 import {signIn, signOut, useSession} from "next-auth/react";
-import { api } from "~/utils/api";
+import { api, type RouterOutputs } from "~/utils/api";
 import Head from "next/head";
 import {Header} from "../components/Header";
 const Home: NextPage = () => {
@@ -21,17 +22,57 @@ const Home: NextPage = () => {
 
 export default Home;
 
+type Topic = RouterOutputs["topic"]["getAll"][0];
+
 const Content: React.FC = () => {
   const { data: sessionData } = useSession();
+  const [selectedTopic, setSelectedTopic] = useState<Topic | null>(null);
   const {data: topics, refetch: refetchTopics} = api.topic.getAll.useQuery(
     undefined,
     {
-      enabled: sessionData?.user !== undefined
+      enabled: sessionData?.user !== undefined,
+      onSuccess: (data) => {
+        setSelectedTopic(selectedTopic ?? data[0] ?? null);
+      }
     }
-  );
+    );
+    const createTopic = api.topic.create.useMutation({
+      onSuccess: () => {
+        refetchTopics();
+      }
+    })
     return (
-    <div>
+    <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
+      <div className="px-2">
+        <ul className="menu rounded-box w-56 bg-base-100 p2">
+          {topics?.map((topic) => (
+            <li key={topic.id}>
+              <a href="#"
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+              >{topic.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="divider"></div>
+        <input 
+        type="text" 
+        placeholder="New Topic"
+        className="input-bordered input input-sm w-full" 
+        onKeyDown={(e) =>{
+          if(e.key === "Enter") {
+            createTopic.mutate({
+              title: e.currentTarget.value
+            });
+            e.currentTarget.value = "";
+          }
+        }}
+        />
+        <div className="col-span-3">
 
+        </div>
+      </div>
     </div>
   );
 };
